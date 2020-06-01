04576b9c0c2619f5c232a172905e72d3b3b0f455
Kokozord <koko_lacteos666@hotmail.com>
2020-06-01 17:35:45 -0500

    add stat pictures

--- client/src/components/unitStats.js
+++ client/src/components/unitStats.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-
 export default class UnitStats extends Component {
   render () {
     let unit = this.props.unit
     return (
       <div className='unit-stats container'>
         <div className='unit-stats header'>Stats</div>
-        <div className='unit-stat'><span>Attack:</span> {unit.attack}</div>
-        <div className='unit-stat'><span>Melee Armor:</span> {unit.armor}</div>
-        <div className='unit-stat'><span>Pierce Armor:</span> {unit.pierce_armor}</div>
-        <div className='unit-stat'><span>HP:</span> {unit.hp}</div>
+        <div className='unit-stat'><img src='/stats/health.png' width='20px' height='20px'/><span>HP: </span> {unit.hp}</div>
+        <div className='unit-stat'><img src='/stats/attack.png' width='20px' height='20px'/><span>Attack: </span> {unit.attack}</div>
+        <div className='unit-stat'><img src='/stats/armor.png' width='20px' height='20px'/><span>Melee Armor:</span> {unit.armor}</div>
+        <div className='unit-stat'><img src='/stats/pierce.png' width='20px' height='20px'/><span>Pierce Armor:</span> {unit.pierce_armor}</div>
       </div>
     )
   }
